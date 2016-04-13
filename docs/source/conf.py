9cb38bdaf72c8f873c792e60b000271b3ed8c3b4
Michael Sobrepera <mjsobrep@live.com>
2016-04-13 14:37:14 -0400

    added png logo and got autodoc working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 
-os.system("sphinx-apidoc -f -o ../build/modules ../../quaternion")
+os.system("sphinx-apidoc -f -o ./_modules ../../quaternion")
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
