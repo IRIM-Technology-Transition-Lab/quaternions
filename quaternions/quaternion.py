c78a733d0b98b923fc1aa0a32c1a9c92eaac0186
Michael Sobrepera <mjsobrep@live.com>
2018-05-05 11:52:21 -0400

    fixed style issues

--- quaternions/quaternion.py
+++ quaternions/quaternion.py
@@ -258,18 +258,18 @@ class Quaternion(object):
         Constructs a quaternion from a rotation vector.
 
         Args:
-            vect: A rotation vector with angle as the magnitude and vector for 
-                  the vector. 
+            vect: A rotation vector with angle as the magnitude and vector for
+                  the vector.
 
         Returns:
             The constructed quaternion
         """
         angle = math.sqrt(sum(element**2 for element in vect))
-        if angle==0:
-            axis = [1,0,0]
+        if angle == 0:
+            axis = [1, 0, 0]
         else:
             axis = [element/angle for element in vect]
-        return cls.from_axis_angle(axis,angle)
+        return cls.from_axis_angle(axis, angle)
 
     def __add__(self, other):
         """
@@ -471,7 +471,7 @@ class Quaternion(object):
             The rotation matrix which this quaternion is equivalent to as a
             list of three lists of three elements each
         """
-        rot_matx = [[0,0,0],[0,0,0],[0,0,0]]
+        rot_matx = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         rot_matx[0][0] = (math.pow(self.w, 2) + math.pow(self.x, 2) -
                           math.pow(self.y, 2) - math.pow(self.z, 2))
         rot_matx[0][1] = (2*self.x*self.y - 2*self.w*self.z)
@@ -495,7 +495,7 @@ class Quaternion(object):
         return Quaternion(-self.w, -self.x, -self.y, -self.z)
 
     def get_euler(self):
-        """Return an euler angle representation of the quaternion. Taken from 
+        """Return an euler angle representation of the quaternion. Taken from
         [wikipedia](https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles)
 
         Returns:
@@ -526,11 +526,11 @@ class Quaternion(object):
     def get_rotation_vector(self):
         angle = math.acos(self.w)*2
         if angle == 0:
-            return [0,0,0]
+            return [0, 0, 0]
         x = angle * self.x/math.sin(angle/2)
         y = angle * self.y/math.sin(angle/2)
         z = angle * self.z/math.sin(angle/2)
-        return [x,y,z]
+        return [x, y, z]
 
     def get_xyz_vector(self):
         return [self.x, self.y, self.z]
@@ -539,11 +539,13 @@ class Quaternion(object):
     def average(quats, init, threshold=0.01):
         qt_bar = init
         dist = 5
-        while dist>threshold:
-            error = [0,0,0]
+        while dist > threshold:
+            error = [0, 0, 0]
             for element in quats:
                 addition = (element*(qt_bar.inverse())).get_rotation_vector()
-                error = [error[idx]+addition[idx] for idx in range(3)] # TODO: take to axis angle for averaging then back to quaternion
+                error = [error[idx]+addition[idx] for idx in range(3)]
+                # TODO: take to axis angle for averaging then back to
+                # quaternion
             error = [element/len(quats) for element in error]
             error = Quaternion.from_rotation_vector(error)
             qt_bar_new = error*qt_bar
